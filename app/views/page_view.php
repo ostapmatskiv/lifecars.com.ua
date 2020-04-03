e3e3afbf8fcf4104d51c74760451cfe780718bf5
Alex <o.darchuk@gmail.com>
2020-04-03 17:42:23 +0300

    template start 1

--- app/views/page_view.php
+++ app/views/page_view.php
@@ -1,8 +1,9 @@
+<!DOCTYPE html>
 <html lang="<?=$_SESSION['language']?>" prefix="og: http://ogp.me/ns#">
 <head>
 	<title><?=$_SESSION['alias']->title?></title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta name="title" content="<?=$_SESSION['alias']->title?>">
     <meta name="description" content="<?=$_SESSION['alias']->description?>">
     <meta name="keywords" content="<?=$_SESSION['alias']->keywords?>">
@@ -18,49 +19,39 @@
 	<?=html_entity_decode($_SESSION['option']->global_MetaTags, ENT_QUOTES)?>
     <?=html_entity_decode($_SESSION['alias']->meta, ENT_QUOTES)?>
 
-	<link rel="shortcut icon" href="<?=SERVER_URL?>style/admin/images/whitelion-black.png">
-
-	<link href="<?=SERVER_URL?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="<?=SERVER_URL?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
-	<link href="<?=SERVER_URL?>style/animate.min.css" rel="stylesheet" />
-	<link href="<?=SERVER_URL?>style/style.min.css" rel="stylesheet" />
-	<link href="<?=SERVER_URL?>style/style-responsive.min.css" rel="stylesheet" />
-	<!-- ================== END BASE CSS STYLE ================== -->
-    
-	<!-- ================== BEGIN BASE JS ================== -->
-	<script src="<?=SERVER_URL?>assets/pace/pace.min.js"></script>
-	<!-- ================== END BASE JS ================== -->
+	<link rel="canonical" href="">
+	<link rel="alternate" hreflang="uk-UA" href="<?=SITE_URL_UK?>">
+	<link rel="alternate" hreflang="ru-UA" href="<?=SITE_URL_RU?>">
+	<link rel="shortcut icon" type="image/x-icon" href="<?=SERVER_URL?>favicon.ico">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=SERVER_URL?>style/ws__main.css">
+	<link rel="stylesheet" type="text/css" href="<?=SERVER_URL?>style/style.css">
 </head>
 <body>
 	<?php
-		echo('<div id="wrapper">');
+		echo('<div class="container">');
 
 		include "@commons/header.php";
 
 		if(isset($view_file)) require_once($view_file.'.php');
 
-		include "@commons/footer.php";
+		// include "@commons/footer.php";
 
 		echo('</div>');
 	?>
-	<script type="text/javascript" src="<?=SERVER_URL?>assets/jquery/jquery-1.9.1.min.js"></script>
-	<script type="text/javascript" src="<?=SERVER_URL?>assets/jquery/jquery-migrate-1.1.0.min.js"></script>
-	<script type="text/javascript" src="<?=SERVER_URL?>assets/bootstrap/js/bootstrap.min.js"></script>
-	<!--[if lt IE 9]>
-		<script src="<?=SERVER_URL?>assets/crossbrowserjs/html5shiv.js"></script>
-		<script src="<?=SERVER_URL?>assets/crossbrowserjs/respond.min.js"></script>
-		<script src="<?=SERVER_URL?>assets/crossbrowserjs/excanvas.min.js"></script>
-	<![endif]-->
-	<script src="<?=SERVER_URL?>assets/jquery-cookie/jquery.cookie.js"></script>
-	<script src="<?=SERVER_URL?>assets/color-admin/apps.min.js"></script>
-	<!-- ================== END BASE JS ================== -->
-	
+	<script type="text/javascript" src="<?=SERVER_URL?>assets/jquery/jquery-3.4.1.min.js"></script>
+	<?php
+		if(!empty($_SESSION['alias']->js_load)) {
+			foreach ($_SESSION['alias']->js_load as $js) {
+				echo '<script type="text/javascript" src="'.SITE_URL.$js.'"></script> ';
+			}
+		}
+	?>
 	<script>
 		var SERVER_URL = '<?=SERVER_URL?>';
 		var SITE_URL = '<?=SITE_URL?>';
 		var ALIAS_URL = '<?=SITE_URL.$_SESSION['alias']->alias?>/';
 	    $(document).ready(function() {
-	        App.init();
 	        <?php
 			if(!empty($_SESSION['alias']->js_init)) {
 				foreach ($_SESSION['alias']->js_init as $js) {
@@ -69,13 +60,6 @@
 			}
 			?>
 	    });
-	</script>
-	<?php
-		if(!empty($_SESSION['alias']->js_load)) {
-			foreach ($_SESSION['alias']->js_load as $js) {
-				echo '<script type="text/javascript" src="'.SITE_URL.$js.'"></script> ';
-			}
-		}
-	?>
+	</script>	
 </body>
 </html>
