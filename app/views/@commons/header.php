7954ac0134eb06cde9837ec8d1a58659305bbaeb
Alex <o.darchuk@gmail.com>
2020-04-06 15:03:09 +0300

    header+fonts

--- app/views/@commons/header.php
+++ app/views/@commons/header.php
@@ -1,3 +1,47 @@
 <header>
-    
+    <div class="flex v-center">
+        <a href="" class="header__link">
+            <img src="style/images/carslogo.png" alt="">
+        </a>
+        <div class="flex">
+            <form class="header__forms">
+                <select size="1">
+                    <option value="t1">Категорії</option>
+                    <option value="t2">Категорії</option>
+                    <option value="t3">Категорії</option>
+                    <option value="t4">Категорії</option>
+                </select>
+                    <i class="fas fa-chevron-down"></i>
+            </form>
+            <div class="header__search">
+                <input required="required" type="search" placeholder="Пошук">
+                <i class="fas fa-search"></i>
+            </div>
+        </div>
+        <div class="flex v-center header__address">
+            <i class="fas fa-map-marker-alt"></i>
+            <address>
+                Львів, вул.<br>
+                Виговського, 49
+            </address>
+        </div>
+        <div class="flex v-center header__phone">
+                <i class="fas fa-phone-alt"></i>
+            <div class="phone__descr">
+                <a href="tel:+380965836762">+38 096 583 67 62</a><br>
+                <a href="tel:+380582359854">+38 058 235 98 54</a>
+            </div>
+         </div>
+        <div class="header__shoping">
+            <i class="fas fa-shopping-cart"></i>
+        </div>
+    </div>
+    <nav class="flex v-center">
+        <a class="nav__link" href="#">Каталог запчастин</a>
+        <a class="nav__link" href="#">Виробники</a>
+        <a class="nav__link" href="#">Повернення та гарантія</a>
+        <a class="nav__link" href="#">Оплата та доставка</a>
+        <a class="nav__link" href="#">Контакти</a>
+        <a class="nav__link" href="#">Реєстрація</a>
+    </nav>
 </header>
