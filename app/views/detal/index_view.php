f95a6dd735952a2a0bf8c2549ae382e7248913e7
Alex <o.darchuk@gmail.com>
2020-05-19 13:59:10 +0300

    accessories

--- app/views/detal/index_view.php
+++ app/views/detal/index_view.php
@@ -1,4 +1,4 @@
-<main class="cars__models">
+<main class="cars__accessories">
     <div class="flex v-center model__menu">
         <a href="#">Маркі та моделі</a>
         <a href="#">Аксесуари</a>
@@ -6,294 +6,145 @@
         <a href="#">Лампочкі</a>
         <a href="#">Акумулятори</a>
     </div>
-  <div class="flex v-center models__geely">
-      <div class="flex v-center geely__info">
-         <div>
-            <img src="style/images/model/12.png" alt="geely">
-         </div>
-          <span>Geely</span>
-      </div>
-      <button class="flex h-center v-center models__geely-btn">
-          <img src="style/icons/model/arrow-down.svg" alt="arrow-down">
-      </button>
-  </div>
-  <div class="flex h-start wrap geely__cars">
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Amulet</div>
+    <div class="flex wrap accessories__list">
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-1.png" alt="Бризговики">
+            <figcaption>Бризговики</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-2.png" alt="Чохли">
+            <figcaption>Чохли</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-10.png" alt="Автокрепеж">
+            <figcaption>Автокрепеж</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-4.png" alt="Авто-електроніка">
+            <figcaption>Авто-електроніка</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-5.png" alt="Аптечки, огнетушители, знаки, жилетки">
+            <figcaption>Аптечки,<br>огнетушители,<br>знаки, жилетки</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-6.png" alt="Ароматизатори">
+            <figcaption>Ароматизатори</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-7.png" alt="Губки,тряпки">
+            <figcaption>Губки,<br>тряпки</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-8.png" alt="Двірники">
+            <figcaption>Двірники</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-9.png" alt="Тримачі">
+            <figcaption>Тримачі</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-10.png" alt="Інструмент">
+            <figcaption>Інструмент</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-11.png" alt="Канистры,лейки">
+            <figcaption>Каністри,<br>лійки</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-12.png" alt="Клипсы">
+            <figcaption>Кліпси</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-13.png" alt="Коврики,липкие,автомобильные">
+            <figcaption>Коврики<br>липкі<br>автомобильні</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-14.png" alt="Краски">
+            <figcaption>Краски</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-15.png" alt="Рамки,номерного,знака">
+            <figcaption>Рамки<br>номерного<br>знака</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-16.png" alt="Тонувальна плівка">
+            <figcaption>Тонувальна<br>плівка</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-17.png" alt="Троси буксирувальні">
+            <figcaption>Троси<br>буксирувальні</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-18.png" alt="Тюнінг">
+            <figcaption>Тюнінг</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-19.png" alt="Форкопи">
+            <figcaption>Форкопи</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-20.png" alt="Хомути">
+            <figcaption>Хомути</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-21.png" alt="Чехлы на руль и КПП">
+            <figcaption>Чехлы на руль<br>и КПП</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-22.png" alt="Шторкисолнцезащитные">
+            <figcaption>Шторки солнцезахистні</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-23.png" alt="Щетки
+            и скребки">
+            <figcaption>Щітки<br>та скребки</figcaption>
+           </figure>
+        </a>
     </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Jaggi</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">QQ</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Elara</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Eastar</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Kimo</div>
-    </div>
- 
-        <div class="base__detal">
-            <img src="style/images/carslogo/cars.png" alt="car">
-            <div class="detal__text">Arrizo 3</div>
-        </div>
-        <div class="base__detal">
-            <img src="style/images/carslogo/cars.png" alt="car">
-            <div class="detal__text">Arrizo 7</div>
-        </div>
-        <div class="base__detal">
-            <img src="style/images/carslogo/cars.png" alt="car">
-            <div class="detal__text">E 5</div>
-        </div>
-        <div class="base__detal">
-            <img src="style/images/carslogo/cars.png" alt="car">
-            <div class="detal__text">Karry</div>
-        </div>
-        <div class="base__detal">
-            <img src="style/images/carslogo/cars.png" alt="car">
-            <div class="detal__text">Tiggo 2</div>
-        </div>
-        <div class="base__detal">
-            <img src="style/images/carslogo/cars.png" alt="car">
-            <div class="detal__text">Tiggo 3</div>
-        </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo 5</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo 7</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">A 13 (ZAZ Forza)</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">M 11</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Beat</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Beat</div>
-    </div>
-  </div>
-  <div class="flex v-center models__chery">
-    <div class="flex v-center chery__info">
-       <div>
-        <img src="style/images/model/2.png" alt="chery">
-       </div>
-        <span>Chery</span>
-    </div>
-    <button class="flex h-center v-center models__chery-btn">
-        <img src="style/icons/model/arrow-down.svg" alt="arrow-down">
-    </button>
-  </div>
-  <div class="flex h-start wrap chery__cars">
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Amulet</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Jaggi</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Amulet</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Jaggi</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">QQ</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Elara</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Eastar</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Kimo</div>
-    </div>
-  </div>
-  <div class="flex v-center models__wall">
-    <div class="flex v-center wall__info">
-        <div>
-            <img src="style/images/model/1.png" alt="wall">
-        </div>
-        <span>Great Wall</span>
-    </div>
-    <button class="flex h-center v-center models__wall-btn">
-        <img src="style/icons/model/arrow-down.svg" alt="arrow-down">
-    </button>
-  </div>
-  <div class="flex h-start wrap wall__cars">
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Amulet</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Jaggi</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Amulet</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Jaggi</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">QQ</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Elara</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Eastar</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Kimo</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Amulet</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Jaggi</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">QQ</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Elara</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Eastar</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Kimo</div>
-    </div>
-  </div>
-  <div class="flex v-center models__lifan">
-    <div class="flex v-center lifan__info">
-        <div>
-            <img src="style/images/model/3.png" alt="lifan">
-        </div>
-        <span>Lifan</span>
-    </div>
-    <button class="flex h-center v-center models__lifan-btn">
-        <img src="style/icons/model/arrow-down.svg" alt="arrow-down">
-    </button>
-  </div>
-  <div class="flex h-start wrap lifan__cars">
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Amulet</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Jaggi</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">QQ</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Elara</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Eastar</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Kimo</div>
-    </div>
-  </div>
-  <div class="flex v-center models__byd">
-    <div class="flex v-center v-center byd__info">
-        <div>
-            <img src="style/images/model/4.png" alt="byd">
-        </div>
-        <span>Byd</span>
-    </div>
-    <button class="flex h-center v-center models__byd-btn">
-        <img src="style/icons/model/arrow-down.svg" alt="arrow-down">
-    </button>
-  </div>
-  <div class="flex h-start wrap byd__cars">
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Amulet</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Tiggo</div>
-    </div>
-    <div class="base__detal">
-        <img src="style/images/carslogo/cars.png" alt="car">
-        <div class="detal__text">Jaggi</div>
-    </div>
-  </div>
 </main>
