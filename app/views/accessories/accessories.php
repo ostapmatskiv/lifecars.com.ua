ce4304fe6e16e85cae1d738d7f495ac7c834dd8c
Alex <o.darchuk@gmail.com>
2020-05-22 11:20:51 +0300

    liquids

---
+++ app/views/accessories/accessories.php
@@ -0,0 +1,150 @@
+<main class="cars__accessories">
+    <div class="flex v-center model__menu">
+        <a href="#">Маркі та моделі</a>
+        <a href="#">Аксесуари</a>
+        <a href="#">Масло та рідина</a>
+        <a href="#">Лампочкі</a>
+        <a href="#">Акумулятори</a>
+    </div>
+    <div class="flex wrap accessories__list">
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-1.png" alt="Бризговики">
+            <figcaption>Бризговики</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-2.png" alt="Чохли">
+            <figcaption>Чохли</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-10.png" alt="Автокрепеж">
+            <figcaption>Автокрепеж</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-4.png" alt="Авто-електроніка">
+            <figcaption>Авто-електроніка</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-5.png" alt="Аптечки, огнетушители, знаки, жилетки">
+            <figcaption>Аптечки,<br>огнетушители,<br>знаки, жилетки</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-6.png" alt="Ароматизатори">
+            <figcaption>Ароматизатори</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-7.png" alt="Губки,тряпки">
+            <figcaption>Губки,<br>тряпки</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-8.png" alt="Двірники">
+            <figcaption>Двірники</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-9.png" alt="Тримачі">
+            <figcaption>Тримачі</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-10.png" alt="Інструмент">
+            <figcaption>Інструмент</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-11.png" alt="Канистры,лейки">
+            <figcaption>Каністри,<br>лійки</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-12.png" alt="Клипсы">
+            <figcaption>Кліпси</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-13.png" alt="Коврики,липкие,автомобильные">
+            <figcaption>Коврики<br>липкі<br>автомобильні</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-14.png" alt="Краски">
+            <figcaption>Краски</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-15.png" alt="Рамки,номерного,знака">
+            <figcaption>Рамки<br>номерного<br>знака</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-16.png" alt="Тонувальна плівка">
+            <figcaption>Тонувальна<br>плівка</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-17.png" alt="Троси буксирувальні">
+            <figcaption>Троси<br>буксирувальні</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-18.png" alt="Тюнінг">
+            <figcaption>Тюнінг</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-19.png" alt="Форкопи">
+            <figcaption>Форкопи</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-20.png" alt="Хомути">
+            <figcaption>Хомути</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-21.png" alt="Чехлы на руль и КПП">
+            <figcaption>Чехлы на руль<br>и КПП</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-22.png" alt="Шторкисолнцезащитные">
+            <figcaption>Шторки солнцезахистні</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/accessories/acces-23.png" alt="Щетки
+            и скребки">
+            <figcaption>Щітки<br>та скребки</figcaption>
+           </figure>
+        </a>
+    </div>
+</main>
--- app/views/detal/index_view.php
+++ app/views/detal/index_view.php
@@ -1,4 +1,4 @@
-<main class="cars__accessories">
+<main class="cars__liquids">
     <div class="flex v-center model__menu">
         <a href="#">Маркі та моделі</a>
         <a href="#">Аксесуари</a>
@@ -6,145 +6,61 @@
         <a href="#">Лампочкі</a>
         <a href="#">Акумулятори</a>
     </div>
-    <div class="flex wrap accessories__list">
+    <div class="flex wrap liquids__list">
         <a href="#"> 
             <figure>
-            <img src="style/images/accessories/acces-1.png" alt="Бризговики">
-            <figcaption>Бризговики</figcaption>
+            <img src="style/images/liquids/camera.png" alt="Бризговики">
+            <figcaption>Масло<br>Моторне</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
-            <img src="style/images/accessories/acces-2.png" alt="Чохли">
-            <figcaption>Чохли</figcaption>
+            <img src="style/images/liquids/camera.png" alt="Чохли">
+            <figcaption>Масло<br>Трансмісійне</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
-            <img src="style/images/accessories/acces-10.png" alt="Автокрепеж">
-            <figcaption>Автокрепеж</figcaption>
+            <img src="style/images/liquids/camera.png" alt="Автокрепеж">
+            <figcaption>Тормозна<br>Жидкість</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
-            <img src="style/images/accessories/acces-4.png" alt="Авто-електроніка">
-            <figcaption>Авто-електроніка</figcaption>
+            <img src="style/images/liquids/camera.png" alt="Авто-електроніка">
+            <figcaption>Омивачі</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
-            <img src="style/images/accessories/acces-5.png" alt="Аптечки, огнетушители, знаки, жилетки">
-            <figcaption>Аптечки,<br>огнетушители,<br>знаки, жилетки</figcaption>
+            <img src="style/images/liquids/camera.png" alt="Аптечки, огнетушители, знаки, жилетки">
+            <figcaption>XADO</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
-            <img src="style/images/accessories/acces-6.png" alt="Ароматизатори">
-            <figcaption>Ароматизатори</figcaption>
+            <img src="style/images/liquids/camera.png" alt="Ароматизатори">
+            <figcaption>Смазки</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
-            <img src="style/images/accessories/acces-7.png" alt="Губки,тряпки">
-            <figcaption>Губки,<br>тряпки</figcaption>
+            <img src="style/images/liquids/camera.png" alt="Губки,тряпки">
+            <figcaption>Автокосметика</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
-            <img src="style/images/accessories/acces-8.png" alt="Двірники">
-            <figcaption>Двірники</figcaption>
+            <img src="style/images/liquids/camera.png" alt="Двірники">
+            <figcaption>Масло<br>для АКПП<br>і ГУРа</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
-            <img src="style/images/accessories/acces-9.png" alt="Тримачі">
-            <figcaption>Тримачі</figcaption>
+            <img src="style/images/liquids/camera.png" alt="Тримачі">
+            <figcaption>Охолоджуюча<br>рідина</figcaption>
            </figure>
         </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-10.png" alt="Інструмент">
-            <figcaption>Інструмент</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-11.png" alt="Канистры,лейки">
-            <figcaption>Каністри,<br>лійки</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-12.png" alt="Клипсы">
-            <figcaption>Кліпси</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-13.png" alt="Коврики,липкие,автомобильные">
-            <figcaption>Коврики<br>липкі<br>автомобильні</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-14.png" alt="Краски">
-            <figcaption>Краски</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-15.png" alt="Рамки,номерного,знака">
-            <figcaption>Рамки<br>номерного<br>знака</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-16.png" alt="Тонувальна плівка">
-            <figcaption>Тонувальна<br>плівка</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-17.png" alt="Троси буксирувальні">
-            <figcaption>Троси<br>буксирувальні</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-18.png" alt="Тюнінг">
-            <figcaption>Тюнінг</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-19.png" alt="Форкопи">
-            <figcaption>Форкопи</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-20.png" alt="Хомути">
-            <figcaption>Хомути</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-21.png" alt="Чехлы на руль и КПП">
-            <figcaption>Чехлы на руль<br>и КПП</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-22.png" alt="Шторкисолнцезащитные">
-            <figcaption>Шторки солнцезахистні</figcaption>
-           </figure>
-        </a>
-        <a href="#"> 
-            <figure>
-            <img src="style/images/accessories/acces-23.png" alt="Щетки
-            и скребки">
-            <figcaption>Щітки<br>та скребки</figcaption>
-           </figure>
-        </a>
+       
     </div>
 </main>
