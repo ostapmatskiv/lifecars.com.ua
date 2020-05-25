64e98e61264c4fcf06ef6d2d7e4d8c3f8d40cdb3
Alex <o.darchuk@gmail.com>
2020-05-25 11:50:22 +0300

    light

--- app/views/liquids___/liquids.php
+++ app/views/liquids___/liquids.php
@@ -1,4 +1,4 @@
-<main class="cars__light">
+<main class="cars__liquids">
     <div class="flex v-center model__menu">
         <a href="#">Маркі та моделі</a>
         <a href="#">Аксесуари</a>
@@ -6,48 +6,61 @@
         <a href="#">Лампочкі</a>
         <a href="#">Акумулятори</a>
     </div>
-    <div class="flex wrap light__list">
+    <div class="flex wrap liquids__list">
         <a href="#"> 
             <figure>
             <img src="style/images/liquids/camera.png" alt="Бризговики">
-            <figcaption>H1</figcaption>
+            <figcaption>Масло<br>Моторне</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
             <img src="style/images/liquids/camera.png" alt="Чохли">
-            <figcaption>H3</figcaption>
+            <figcaption>Масло<br>Трансмісійне</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
             <img src="style/images/liquids/camera.png" alt="Автокрепеж">
-            <figcaption>H4</figcaption>
+            <figcaption>Тормозна<br>Жидкість</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
             <img src="style/images/liquids/camera.png" alt="Авто-електроніка">
-            <figcaption>H7</figcaption>
+            <figcaption>Омивачі</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
             <img src="style/images/liquids/camera.png" alt="Аптечки, огнетушители, знаки, жилетки">
-            <figcaption>H8</figcaption>
+            <figcaption>XADO</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
             <img src="style/images/liquids/camera.png" alt="Ароматизатори">
-            <figcaption>H11</figcaption>
+            <figcaption>Смазки</figcaption>
            </figure>
         </a>
         <a href="#"> 
             <figure>
             <img src="style/images/liquids/camera.png" alt="Губки,тряпки">
-            <figcaption>Інші</figcaption>
+            <figcaption>Автокосметика</figcaption>
            </figure>
         </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/liquids/camera.png" alt="Двірники">
+            <figcaption>Масло<br>для АКПП<br>і ГУРа</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/liquids/camera.png" alt="Тримачі">
+            <figcaption>Охолоджуюча<br>рідина</figcaption>
+           </figure>
+        </a>
+       
     </div>
 </main>
