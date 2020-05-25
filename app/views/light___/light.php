1d45992b8ac381c403afc9dbf8cead3cf7e61b8f
Alex <o.darchuk@gmail.com>
2020-05-25 11:51:32 +0300

    light.php

---
+++ app/views/light___/light.php
@@ -0,0 +1,53 @@
+<main class="cars__light">
+    <div class="flex v-center model__menu">
+        <a href="#">Маркі та моделі</a>
+        <a href="#">Аксесуари</a>
+        <a href="#">Масло та рідина</a>
+        <a href="#">Лампочкі</a>
+        <a href="#">Акумулятори</a>
+    </div>
+    <div class="flex wrap light__list">
+        <a href="#"> 
+            <figure>
+            <img src="style/images/liquids/camera.png" alt="Бризговики">
+            <figcaption>H1</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/liquids/camera.png" alt="Чохли">
+            <figcaption>H3</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/liquids/camera.png" alt="Автокрепеж">
+            <figcaption>H4</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/liquids/camera.png" alt="Авто-електроніка">
+            <figcaption>H7</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/liquids/camera.png" alt="Аптечки, огнетушители, знаки, жилетки">
+            <figcaption>H8</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/liquids/camera.png" alt="Ароматизатори">
+            <figcaption>H11</figcaption>
+           </figure>
+        </a>
+        <a href="#"> 
+            <figure>
+            <img src="style/images/liquids/camera.png" alt="Губки,тряпки">
+            <figcaption>Інші</figcaption>
+           </figure>
+        </a>
+    </div>
+</main>
