16a734ce09c269b95b43a2cdf665235687ec2987
vitalyrepeta <wrepeta@ukr.net>
2020-07-07 16:36:08 +0300

    new header

--- app/views/@commons/header.php
+++ app/views/@commons/header.php
@@ -1,3 +1,30 @@
+<div class="flex v-center" id="top_header">
+    <div class="header__address">
+        <?php /* <i class="fas fa-map-marker-alt"></i>
+        <a href="https://goo.gl/maps/ZVp3Y1JQkubKnLBR9" target="_blank">Львів, вул. Виговського, 49</a> */ ?>
+    </div>
+    <div>
+        <div class="language">
+            <a href="<?=SITE_URL_UK?>" <?=$_SESSION['language'] == 'uk' ? 'class="active"' : ''?>>UA</a> | <a href="<?=SITE_URL_RU?>" <?=$_SESSION['language'] == 'ru' ? 'class="active"' : ''?>>RU</a>
+        </div>
+        <div class="profile">
+            <?php if($this->userIs()) { ?>
+                Вітаємо, 
+                <a href="<?=SITE_URL?>profile" class="active" title="Мій кабінет">
+                    <img src="<?=SERVER_URL?>style/icons/user.png" alt="user">
+                    <?=$_SESSION['user']->name?>
+                </a>
+                <?php if($this->userCan()) { ?>
+                    <a href="/admin" class="admin">Admin</a>
+                <?php } ?>
+                <a href="/logout"><?=$this->text('Вийти', 0)?></a>
+            <?php } else { ?>
+                <a href="<?=SITE_URL?>login"><?=$this->text('Увійти', 0)?></a>
+                <a href="<?=SITE_URL?>signup"><?=$this->text('Реєстрація', 0)?></a>
+            <?php } ?>
+        </div>
+    </div>
+</div>
 <header>
     <div class="flex v-center">
         <div class="nav__mobile">
@@ -17,7 +44,7 @@
                 $selected_id = $this->data->get('group');
                 ?>
                 <select name="group">
-                    <option value="0">Весь магазин</option>
+                    <option value="0">Де шукати?</option>
                     <?php foreach ($catalogAllGroups as $h_group) {
                         if($h_group->parent == 0) {
                             $selected = $selected_id == $h_group->id ? 'selected' : '';
@@ -33,16 +60,17 @@
                 </select>
                 <i class="fas fa-chevron-down"></i>
             <?php } ?>
-            <input required="required" type="search" name="name" value="<?=$this->data->get('name')?>" placeholder="Пошук">
+            <input required="required" type="search" name="name" value="<?=$this->data->get('name')?>" placeholder="Пошук за артикулом або назвою товару">
             <button><i class="fas fa-search"></i></button>
         </form>
+        <?php /* ?>
         <div class="flex v-center header__address">
             <i class="fas fa-map-marker-alt"></i>
             <address>
                 <a href="https://goo.gl/maps/ZVp3Y1JQkubKnLBR9" target="_blank">Львів, вул.<br>
                 Виговського, 49</a>
             </address>
-        </div>
+        </div> */ ?>
         <div class="flex v-center header__phone">
             <i class="fas fa-phone-alt"></i>
             <div>
@@ -58,7 +86,7 @@
             <a href="<?=SITE_URL?>cart" class="header__cart">
                 <img src="<?=SERVER_URL?>style/icons/shopping-cart-16.png" alt="cart">
             </a>
-            <?php if($this->userIs()) { ?>
+            <?php /* if($this->userIs()) { ?>
                 <a href="<?=SITE_URL?>profile" class="header__user" title="Мій кабінет">
                     <img src="<?=SERVER_URL?>style/icons/user.png" alt="user">
                 </a>
@@ -66,7 +94,7 @@
                 <a href="<?=SITE_URL?>signup" class="header__user" title="Реєстрація">
                     <img src="<?=SERVER_URL?>style/icons/user.png" alt="user">
                 </a>
-            <?php } ?>
+            <?php } */ ?>
         </div>
     </div>
     <nav class="flex v-center">
@@ -75,13 +103,13 @@
         <a href="<?=SITE_URL?>exchange-and-return" <?=($_SESSION['alias']->alias == 'exchange-and-return') ? 'class="active"' : ''?>>Повернення та гарантія</a>
         <a href="<?=SITE_URL?>delivery-and-payments" <?=($_SESSION['alias']->alias == 'delivery-and-payments') ? 'class="active"' : ''?>>Оплата та доставка</a>
         <a href="<?=SITE_URL?>contacts" <?=($_SESSION['alias']->alias == 'contacts') ? 'class="active"' : ''?>>Контакти</a>
-        <?php if($this->userIs()) { if($this->userCan()) { ?>
+        <?php /* if($this->userIs()) { if($this->userCan()) { ?>
             <a href="<?=SITE_URL?>admin">Admin</a>
         <?php } else { ?>
             <a href="<?=SITE_URL?>profile" <?=($_SESSION['alias']->alias == 'profile') ? 'class="active"' : ''?>>Мій кабінет</a>
         <?php } } else { ?>
             <a href="<?=SITE_URL?>login" <?=(in_array($_SESSION['alias']->alias, ['login', 'signup'])) ? 'class="active"' : ''?>>Увійти / Реєстрація</a>
-        <?php } ?>
+        <?php } */ ?>
     </nav>
     <div class="mob__menu">
         <div class="close__menu">
