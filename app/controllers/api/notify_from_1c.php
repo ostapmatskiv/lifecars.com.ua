6da3512accdfe2d23dead46299863b4be0766ccc
Ostap Matskiv <ostap.matskiv@gmail.com>
2020-07-23 12:57:08 +0300

    notify_from_1c. fix cart

--- app/controllers/api/notify_from_1c.php
+++ app/controllers/api/notify_from_1c.php
@@ -9,10 +9,27 @@ class notify_from_1c extends Controller
 	
 	function index()
 	{
+		$time = time();
 		$alias_table = ['cart' => 's_cart'];
 		foreach ($alias_table as $alias => $table) {
-			if($value = $this->data->get($alias))
-				echo "ok: cart ".$value;
+			if($row_id = $this->data->get($alias))
+			{
+				$data = ['date_1c' => $time];
+				if($alias == 'returns')
+					$data = ['date_synchronization' => $time];
+				if($alias == 'cart')
+				{
+					if($cart = $this->db->getAllDataById($table, $row_id))
+						$data['1c_status'] = $cart->status;
+					else
+					{
+						echo "error: cart #".$row_id.' not found';
+						exit;
+					}
+				}
+				if($this->db->updateRow($table, $data, $row_id))
+					echo "ok: {$alias} #".$row_id;
+			}
 		}
 	}
 
