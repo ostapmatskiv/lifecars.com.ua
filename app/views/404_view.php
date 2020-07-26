faa45892851540a255e48414eb2a422da4a7c24d
Ostap Matskiv <ostap.matskiv@gmail.com>
2020-07-26 18:32:32 +0300

    404

--- app/views/404_view.php
+++ app/views/404_view.php
@@ -1,15 +1,6 @@
-<link rel="stylesheet" href="<?=SERVER_URL?>style/css/page_404_error.css">
-<div class="container content">
-    <!--Error Block-->
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="error-v1">
-                <span class="error-v1-title">404</span>
-                <span>That’s an error!</span>
-                <p>The requested URL was not found on this server. That’s all we know.</p>
-                <a class="btn-u btn-bordered" href="<?=SITE_URL?>">На головну</a>
-            </div>
-        </div>
-    </div>
-    <!--End Error Block-->
+<div class="container">
+    <h2 style="font-size: 43px;"><i class="fas fa-exclamation-triangle"></i> 404 Сторінку не знайдено</h2>
+    <h3 style="font-size: 27px;">Можливо, вказано некоректний адрес або сторінку видалено</h3>
+    <br>
+    <a class="btn" href="<?=SITE_URL?>">На головну</a>
 </div>
