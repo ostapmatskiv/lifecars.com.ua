8f50ef70c1a9da4b9d0bda855eeba6fe7c0551f6
Ostap Matskiv <ostap.matskiv@gmail.com>
2020-08-03 10:58:31 +0300

    contacts data

--- app/views/contacts/index_view.php
+++ app/views/contacts/index_view.php
@@ -2,25 +2,23 @@
 <?php $this->load->js('assets/slick/slick.min.js'); 
 $this->load->js_init('init__main()'); ?>
 
-
-
 <main>
-    <h1>Контакти</h1>
+    <h1><?=$_SESSION['alias']->name?></h1>
     <section id="contacts" class="flex m-column container">
         <div class="w50 m100 contactsText">
             <h2>Зв'яжіться з нами:</h2>
             <div>
-                <p><i class="fas fa-map-marker-alt"></i> Львів, вул. Виговського, 49</p>
+                <p><i class="fas fa-map-marker-alt"></i> <?=$this->text('м. Львів, вул. Виговського, 49')?></p>
             </div>
             <div>
                 <p><i class="fas fa-phone-alt"></i> <a href="telto:+38 096 0000 943">+38 096 0000 943</a></p>
                 <p><i class="fas fa-phone-alt iNone"></i> <a href="telto:+38 093 0000 943">+38 093 0000 943</a></p>
             </div>
             <div>
-                <p><i class="fas fa-envelope"></i> <a href="mailto:email@.com">email@.com</a></p>
+                <p><i class="fas fa-envelope"></i> <a href="mailto:manager@lifecars.com.ua">manager@lifecars.com.ua</a></p>
             </div>
             <div class="contactsMap">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2574.407265598625!2d23.971396515239693!3d49.816010240756086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ae772e6137b4b%3A0x83c45259538886f8!2z0LLRg9C70LjRhtGPINCG0LLQsNC90LAg0JLQuNCz0L7QstGB0YzQutC-0LPQviwgNDksINCb0YzQstGW0LIsINCb0YzQstGW0LLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgNzkwMDA!5e0!3m2!1suk!2sua!4v1594740792165!5m2!1suk!2sua" width="480" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5148.814876012718!2d23.973585!3d49.816007!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa5abad275ae2f1ff!2sLifeCars!5e0!3m2!1suk!2sua!4v1596440989263!5m2!1suk!2sua" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
         </div>
         <div class="w50 m100 contactsForm">
