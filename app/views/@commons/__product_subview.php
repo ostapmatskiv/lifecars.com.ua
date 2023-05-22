53b4c9d021d6db078d1502b89410f1318d2eb88f
Ostap Matskiv <levso7@gmail.com>
2023-05-22 14:58:24 +0300

    product subview stars gold

--- app/views/@commons/__product_subview.php
+++ app/views/@commons/__product_subview.php
@@ -118,7 +118,7 @@
         </div>
         <div class="flex v-center card__rating">
             <?php if(empty($product->rating)) $product->rating = 5; ?>
-            <div class="rating <?=empty($product->rating)?'empty':''?>" title="<?=empty($product->rating)?$this->text('Оцінка відсутня'):$this->text('Оцінка товару ').' '.$product->rating?>" style="color: #ffc508;">
+            <div class="rating <?=empty($product->rating)?'empty':''?>" title="<?=empty($product->rating)?$this->text('Оцінка відсутня'):$this->text('Оцінка товару ').' '.$product->rating?>">
                 <?php for($i = 0; $i < round($product->rating); $i++) { ?>
                     <i class="fas fa-star" aria-hidden="true"></i>
                 <?php } for($i = round($product->rating); $i < 5; $i++) { ?>
