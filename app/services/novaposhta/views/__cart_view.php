50ca5cdb69ab6412a5d78a7902d2855b6b35558c
Ostap Matskiv <levso7@gmail.com>
2023-06-24 22:09:23 +0300

    вибір доставки

--- app/services/novaposhta/views/__cart_view.php
+++ app/services/novaposhta/views/__cart_view.php
@@ -1,84 +1,95 @@
-<?php /* <div id="nova-poshta-method" class="flex">
-    <div data-tab="warehouse" class="w50<?=(!empty($userShipping->method) && $userShipping->method == 'courier') ? '':' active'?>"><?=$this->text('На відділення')?></div>
-    <div data-tab="courier" class="w50<?=(!empty($userShipping->method) && $userShipping->method == 'courier') ? ' active':''?>"><?=$this->text("Кур'єром")?></div>
-</div> */ ?>
-
-<input type="hidden" name="nova-poshta-method" value="warehouse">
-<input type="hidden" name="nova-poshta-city-ref" value="<?=$userShipping->city_ref ?? ''?>">
-<input type="hidden" name="nova-poshta-warehouse-ref" value="<?=$userShipping->warehouse_ref ?? ''?>">
-
-<div class="input-group <?= (!empty($userShipping->city_ref) && !empty($userShipping->city)) ? 'val' : '' ?>">
-    <label for="novaposhta-city"><?=$this->text('Місто')?></label>
-    <input type="text" id="novaposhta-city" name="novaposhta-city" placeholder="<?=$this->text('Місто')?>" value="<?=(!empty($userShipping->city_ref) && !empty($userShipping->city)) ? $userShipping->city : ''?>" autocomplete="off" required>
+<div style="margin-left: 10px; padding-left: 5px; border-left: 1px solid #ccc">
+    <?php foreach (['warehouse' => 'На відділення', 'postomat' => 'Поштомат', 'courier' => "Кур'єром"] as $key => $key_title) {
+        $checked = (!empty($userShipping->method) && $userShipping->method == $key) ? '' : 'checked'; ?>
+        <label <?= $checked ? 'class="active"' : '' ?>>
+            <input type="radio" name="nova-poshta-method" value="<?= $key ?>" <?= $checked ?>>
+            <?= $this->text($key_title) ?>
+        </label>
+    <?php } ?>
 </div>
 
-<div id="nova-poshta-warehouse" <?=(!empty($userShipping->method) && $userShipping->method == 'courier') ? 'class="hide"':''?>>
+
+
+<input type="hidden" name="nova-poshta-city-ref" value="<?= $userShipping->city_ref ?? '' ?>">
+<input type="hidden" name="nova-poshta-warehouse-ref" value="<?= $userShipping->warehouse_ref ?? '' ?>">
+
+<div class="input-group <?= (!empty($userShipping->city_ref) && !empty($userShipping->city)) ? 'val' : '' ?>">
+    <label for="novaposhta-city"><?= $this->text('Місто') ?></label>
+    <input type="text" id="novaposhta-city" name="novaposhta-city" placeholder="<?= $this->text('Місто') ?>" value="<?= (!empty($userShipping->city_ref) && !empty($userShipping->city)) ? $userShipping->city : '' ?>" autocomplete="off" required>
+</div>
+
+<div id="nova-poshta-warehouse" <?= (!empty($userShipping->method) && $userShipping->method == 'courier') ? 'class="hide"' : '' ?>>
     <div class="input-group val">
-        <label><?=$this->text('Відділення')?></label>
-        <select name="nova-poshta-warehouse" class="" <?=(!empty($userShipping->method) && $userShipping->method == 'courier') ? '':'required'?>>
+        <label><?= $this->text('Відділення') ?></label>
+        <select name="nova-poshta-warehouse" class="" <?= (!empty($userShipping->method) && $userShipping->method == 'courier') ? '' : 'required' ?>>
             <?php $info = '';
             if (!empty($userShipping->city_ref) && !empty($userShipping->warehouse_ref)) {
-                if($warehouses = $this->getWarehouses($userShipping->city_ref))
+                if ($warehouses = $this->getWarehouses($userShipping->city_ref))
                     foreach ($warehouses as $warehouse) {
                         $selected = '';
-                        if($warehouse->id == $userShipping->warehouse_ref)
-                        {
+                        if ($warehouse->id == $userShipping->warehouse_ref) {
                             $selected = 'selected';
                             $info = $warehouse->info;
                         }
-                        echo '<option data-id="'.$warehouse->id.'" data-info="'.htmlspecialchars($warehouse->info).'" title="'.$warehouse->title.'" '.$selected.'>'.$warehouse->name.'</option>';
+                        echo '<option data-id="' . $warehouse->id . '" data-info="' . htmlspecialchars($warehouse->info) . '" title="' . $warehouse->title . '" ' . $selected . '>' . $warehouse->name . '</option>';
                     }
             } else { ?>
-                <option selected disabled value=""><?=$this->text('Для вибору відділення спершу введіть місто')?></option>
+                <option selected disabled value=""><?= $this->text('Для вибору відділення спершу введіть місто') ?></option>
             <?php } ?>
         </select>
 
-        <div class="info <?=empty($info) ? 'hide' : ''?>"><?=$info?></div>
+        <div class="info <?= empty($info) ? 'hide' : '' ?>"><?= $info ?></div>
     </div>
 </div>
 
-<div id="nova-poshta-courier" <?=(!empty($userShipping->method) && $userShipping->method == 'courier') ? '':'class="hide"'?>>
-    <!-- <label><?=$this->text('Вулиця')?></label> -->
-    <input type="text" name="novaposhta-address-street" class="form-control" placeholder="<?=$this->text('Вулиця')?>" value="<?=$userShipping->address_street ?? ''?>" <?=(!empty($userShipping->method) && $userShipping->method == 'courier') ? 'required':''?>>
-    <input type="text" name="novaposhta-address-house" class="form-control" placeholder="<?=$this->text('Номер будинку/та квартри')?>" value="<?=$userShipping->address_house ?? ''?>" <?=(!empty($userShipping->method) && $userShipping->method == 'courier') ? 'required':''?>>
+<div id="nova-poshta-courier" <?= (!empty($userShipping->method) && $userShipping->method == 'courier') ? '' : 'class="hide"' ?>>
+    <!-- <label><?= $this->text('Вулиця') ?></label> -->
+    <input type="text" name="novaposhta-address-street" class="form-control" placeholder="<?= $this->text('Вулиця') ?>" value="<?= $userShipping->address_street ?? '' ?>" <?= (!empty($userShipping->method) && $userShipping->method == 'courier') ? 'required' : '' ?>>
+    <input type="text" name="novaposhta-address-house" class="form-control" placeholder="<?= $this->text('Номер будинку/та квартри') ?>" value="<?= $userShipping->address_house ?? '' ?>" <?= (!empty($userShipping->method) && $userShipping->method == 'courier') ? 'required' : '' ?>>
 </div>
 
 
 
 <?php $novaposhta_selected = $this->data->re_post('shipping-novaposhta');
-if(empty($novaposhta_selected) && $userShipping && $userShipping->department)
+if (empty($novaposhta_selected) && $userShipping && $userShipping->department)
     $novaposhta_selected = $userShipping->department;
-if($userShipping && $userShipping->initShipping)
+if ($userShipping && $userShipping->initShipping)
     $this->load->js_init('initShipping()'); ?>
 <script>
-    window.onload = function ( ) {
-        
+    window.onload = function() {
+
     };
+
     function setCity(city) {
         $("#nova-poshta-warehouse select").empty().append('<option selected disabled="" value="">Виберіть відділення</option>');
         $("#nova-poshta-warehouse .info").addClass('hide');
         $('input[name="nova-poshta-city-ref"]').val(city);
-        if($('input[name="nova-poshta-method"]').val() == 'warehouse')
-        {
+        if ($('input[name="nova-poshta-method"]').val() == 'warehouse') {
             $("div#divLoading").addClass('show');
             $.ajax({
-                url: '<?=SITE_URL.$_SESSION['alias']->alias?>/getWarehouses',
+                url: '<?= SITE_URL . $_SESSION['alias']->alias ?>/getWarehouses',
                 type: 'POST',
-                data: { 'city' : city },
-                success:function(warehouses) {
-                    if(warehouses)
+                data: {
+                    'city': city
+                },
+                success: function(warehouses) {
+                    if (warehouses)
                         warehouses.forEach(function(warehous) {
-                            $('<option/>', { text: warehous.name, title: warehous.title, 'data-id': warehous.id, 'data-info': warehous.info}).appendTo($("#nova-poshta-warehouse select"))
+                            $('<option/>', {
+                                text: warehous.name,
+                                title: warehous.title,
+                                'data-id': warehous.id,
+                                'data-info': warehous.info
+                            }).appendTo($("#nova-poshta-warehouse select"))
                         });
                 },
                 complete: function() {
                     $("div#divLoading").removeClass('show');
                 }
             })
-        }
-        else
+        } else
             $('input[name="novaposhta-address-street"]').autocomplete({
-                source: '<?=SITE_URL.$_SESSION['alias']->alias?>/getAddresses/'+city,
+                source: '<?= SITE_URL . $_SESSION['alias']->alias ?>/getAddresses/' + city,
                 minLength: 2
             });
     }
@@ -87,30 +98,27 @@ if($userShipping && $userShipping->initShipping)
         $('input[name="nova-poshta-method"]').attr('required', true);
 
         $('input[name="novaposhta-city"]').autocomplete({
-            source: '<?=SITE_URL.$_SESSION['alias']->alias?>/getcities/warehouse',
+            source: '<?= SITE_URL . $_SESSION['alias']->alias ?>/getcities/warehouse',
             minLength: 3,
-            select: function (event, ui) {
+            select: function(event, ui) {
                 $('input[name="novaposhta-city"]').val(ui.item.value);
                 setCity(ui.item.id);
             }
         }).attr('autocomplete', 'none');
-        $('#nova-poshta-method div').click(function(){
+        $('#nova-poshta-method div').click(function() {
             $(this).parent().find('div').removeClass('active');
             $(this).addClass('active');
             var tab = $(this).data('tab');
             $('input[name="nova-poshta-method"]').val(tab);
             $('input[name="nova-poshta-city-ref"], input[name="nova-poshta-warehouse-ref"], input[name="novaposhta-city"], input[name="novaposhta-address-street"]').val('');
-            if(tab == 'warehouse')
-            {
-                $('input[name="novaposhta-city"]').autocomplete( "option", "source", '<?=SITE_URL.$_SESSION['alias']->alias?>/getcities/warehouse');
+            if (tab == 'warehouse') {
+                $('input[name="novaposhta-city"]').autocomplete("option", "source", '<?= SITE_URL . $_SESSION['alias']->alias ?>/getcities/warehouse');
                 $('#nova-poshta-warehouse').removeClass('hide');
                 $('#nova-poshta-courier').addClass('hide');
                 $('select[name="nova-poshta-warehouse"]').attr('required', true);
                 $('input[name="novaposhta-address-street"], input[name="novaposhta-address-house"]').attr('required', false);
-            }
-            else
-            {
-                $('input[name="novaposhta-city"]').autocomplete( "option", "source", '<?=SITE_URL.$_SESSION['alias']->alias?>/getcities/courier');
+            } else {
+                $('input[name="novaposhta-city"]').autocomplete("option", "source", '<?= SITE_URL . $_SESSION['alias']->alias ?>/getcities/courier');
                 $('#nova-poshta-warehouse').addClass('hide');
                 $('#nova-poshta-courier').removeClass('hide');
                 $('select[name="nova-poshta-warehouse"]').attr('required', false);
@@ -137,16 +145,39 @@ if($userShipping && $userShipping->initShipping)
 </script>
 
 <style>
-    #nova-poshta-method { border: 2px solid #7cbe49; border-radius: 2px; margin: 5px 0 }
-    #nova-poshta-method div { padding: 5px; text-align: center; cursor: pointer }
-    #nova-poshta-method div.active { background: #7cbe49; color: #fff }
-    #nova-poshta-method div:hover { text-decoration: underline }
-    #buyer { margin: 15px 0 }
+    #nova-poshta-method {
+        border: 2px solid #7cbe49;
+        border-radius: 2px;
+        margin: 5px 0
+    }
+
+    #nova-poshta-method div {
+        padding: 5px;
+        text-align: center;
+        cursor: pointer
+    }
+
+    #nova-poshta-method div.active {
+        background: #7cbe49;
+        color: #fff
+    }
+
+    #nova-poshta-method div:hover {
+        text-decoration: underline
+    }
+
+    #buyer {
+        margin: 15px 0
+    }
+
     #nova-poshta-warehouse .info {
         border: #7cbe49 1px solid;
         padding: 5px 10px;
         border-radius: 2px;
         display: none;
     }
-    #cart input.ui-autocomplete-loading { background: #eee url("<?=SERVER_URL?>style/images/ui-anim_basic_16x16.gif") right center no-repeat !important }
+
+    #cart input.ui-autocomplete-loading {
+        background: #eee url("<?= SERVER_URL ?>style/images/ui-anim_basic_16x16.gif") right center no-repeat !important
+    }
 </style>
