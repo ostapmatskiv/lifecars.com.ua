306fe40477a34037dffa63ab6bf4d6835017467d
Ostap Matskiv <levso7@gmail.com>
2023-06-24 20:10:31 +0300

    Корзина. Купон як частина форми (знизу)

--- app/services/cart/views/checkout_view.php
+++ app/services/cart/views/checkout_view.php
@@ -27,98 +27,28 @@ if ($products) {
 
 	<div class="flex w100">
 		<div class="w30 m100">
-			<?php if ($bonusCodes && $bonusCodes->showForm) { ?>
-				<h4><?= $this->text('Маєте купон на знижку?') ?></h4>
-				<form action="<?= SITE_URL . $_SESSION['alias']->alias ?>/coupon" method="POST" class="coupon-form text-center">
-					<div class="input-group">
-						<input type="text" name="code" placeholder="<?= $this->text('Код купону') ?>" required>
-						<label for="code"><?= $this->text('Код купону') ?></label>
-					</div>
-					<button class=""><?= $this->text('Застосувати купон') ?></button>
-				</form>
-			<?php }
 
-			/* ?>
-			<div id="percents" data-margin-top="0"><div class="active"></div><div class="text">15%</div></div>
-			<div class="info" data-margin-top="27"><?=$this->text('Статус заповнення інформації')?></div>
+			<?php if (!$this->userIs()) { ?>
+				<h4><?= $this->text('Покупець') ?></h4>
 
-			<?php */
-			if (!$this->userIs()) { /* ?>
-				<div id="cart-signup" class="flex">
-					<div data-tab="new-buyer" class="w50 active"><?=$this->text('Я новий покупець')?></div>
-					<div data-tab="regular-buyer" class="w50"><?=$this->text('Я постійний покупець')?></div>
-				</div>
-				*/ ?>
-
-				<div id="new-buyer">
-					<h4><?= $this->text('Покупець') ?></h4>
-
-					<div class="input-group">
-						<input id="phone" type="text" value="<?= $this->data->re_post('phone') ?>" required minlength="17" />
-						<label for="phone"><?= $this->text('Номер телефону', 5) ?></label>
-						<h5 class="text-danger hide" id="phoneError"><?= $this->text('Введіть коректний номер телефону починаючи +380') ?></h5>
-					</div>
-
-					<div class="input-group">
-						<input id="first_name" type="text" value="<?= $this->data->re_post('first_name') ?>" required />
-						<label for="first_name"><?= $this->text('Ім\'я', 5) ?></label>
-						<h5 class="text-danger hide"><?= $this->text('Тільки кирилиця') ?></h5>
-					</div>
-
-					<div class="input-group">
-						<input id="last_name" type="text" value="<?= $this->data->re_post('last_name') ?>" required />
-						<label for="last_name"><?= $this->text('Прізвище', 5) ?></label>
-						<h5 class="text-danger hide"><?= $this->text('Тільки кирилиця') ?></h5>
-					</div>
+				<div class="input-group">
+					<input id="phone" type="text" value="<?= $this->data->re_post('phone') ?>" required minlength="17" />
+					<label for="phone"><?= $this->text('Номер телефону', 5) ?></label>
+					<h5 class="text-danger hide" id="phoneError"><?= $this->text('Введіть коректний номер телефону починаючи +380') ?></h5>
 				</div>
 
-				<?php /*
-				<div id="regular-buyer" class="hide">
-					<h4><?=$this->text('Вже купували?')?></h4>
-		 			<p><?=$this->text('Увійдіть - це заощадить Ваш час')?></p>
-					<form action="<?=SITE_URL.$_SESSION['alias']->alias?>/login" method="POST">
-						<p class="message"></p>
-						<input type="text" name="email" placeholder="<?=$this->text('email або телефон')?>*" value="<?=$this->data->re_post('email')?>">
-						<input type="password" name="password" placeholder="<?=$this->text('Пароль')?>*">
-						<div>
-							<a href="<?=SITE_URL?>reset" class="right"><?=$this->text('Забув пароль?')?></a>
-							<button type="submit"><?=$this->text('Увійти')?></button>
-						</div>
-					</form>
+				<div class="input-group">
+					<input id="first_name" type="text" value="<?= $this->data->re_post('first_name') ?>" required />
+					<label for="first_name"><?= $this->text('Ім\'я', 5) ?></label>
+					<h5 class="text-danger hide"><?= $this->text('Тільки кирилиця') ?></h5>
 				</div>
-				*/ ?>
 
-				<?php $this->load->library('facebook');
-				if (false && $_SESSION['option']->facebook_initialise) { ?>
-					<div class="facebook">
-						<p><?= $this->text('Швидкий вхід:') ?></p>
-						<button class="facebookSignUp" onclick="return facebookSignUp()">Facebook <i class="fab fa-facebook"></i></button>
-					</div>
-
-					<script>
-						window.fbAsyncInit = function() {
-
-							FB.init({
-								appId: '<?= $this->facebook->getAppId() ?>',
-								cookie: true,
-								xfbml: true,
-								version: 'v2.6'
-							});
-						};
-
-						(function(d, s, id) {
-							var js, fjs = d.getElementsByTagName(s)[0];
-							if (d.getElementById(id)) {
-								return;
-							}
-							js = d.createElement(s);
-							js.id = id;
-							js.src = "//connect.facebook.net/en_US/sdk.js";
-							fjs.parentNode.insertBefore(js, fjs);
-						}(document, 'script', 'facebook-jssdk'));
-					</script>
-			<?php }
-			} ?>
+				<div class="input-group">
+					<input id="last_name" type="text" value="<?= $this->data->re_post('last_name') ?>" required />
+					<label for="last_name"><?= $this->text('Прізвище', 5) ?></label>
+					<h5 class="text-danger hide"><?= $this->text('Тільки кирилиця') ?></h5>
+				</div>
+			<?php } ?>
 
 			<form id="confirm" action="<?= SITE_URL . $_SESSION['alias']->alias ?>/confirm" method="POST">
 				<?php if (!$this->userIs()) { ?>
@@ -163,47 +93,58 @@ if ($products) {
 
 				<h4><?= $this->text('Коментар') ?></h4>
 				<textarea name="comment" class="form-control" placeholder="<?= $this->text('Побажання до замовлення, наприклад щодо доставки') ?>" rows="5"><?= $this->data->re_post('comment') ?></textarea>
-
-				<div class="price-box" data-margin-top="110">
-					<?php if ($bonusCodes && !empty($bonusCodes->info)) { ?>
-						<p class="bonusCode">
-							<?= $this->text('Разом') . ': ' ?>
-							<strong class="right"><?= $subTotalFormat ?></strong>
-						</p>
-						<?php foreach ($bonusCodes->info as $key => $discount) { ?>
-							<p class="bonusCode discount">
-								<?= $this->text('Бонус-код') . ': ' . $key ?>
-								<strong class="right"><?= $discount ?></strong>
-							</p>
-						<?php }
-					}
-					if ($shippings && $shippings[0]->pay >= -1) { ?>
-						<p class="shipping">
-							<?= $this->text('Доставка') ?>
-							<strong class="right"><?= ($shippings[0]->pay == -1) ? $this->text('безкоштовно') : $shippings[0]->priceFormat ?></strong>
-						</p>
-					<?php } ?>
-
-					<?php if ($discountTotal && false) { ?>
-						<p class="discount"><?= $this->text('Ви економите') ?> <strong class="right"><?= $discountTotalFormat ?></strong></p>
-					<?php } ?>
-
-					<p class="price">
-						<?= $this->text('До оплати') ?>
-						<strong class="right"><?= $totalFormat ?></strong>
-					</p>
-
-					<?php if (!empty($_SESSION['option']->dogovirOfertiLink)) { ?>
-						<label id="oferta">
-							<input type="checkbox" name="oferta" <?= $this->userIs() ? 'checked' : '' ?> required>
-							<i class="<?= $this->userIs() ? 'fas fa-check-square' : 'far fa-square' ?>"></i>
-							<div><?= $this->text('Я погоджуюся з') ?> <a href="<?= SERVER_URL . $_SESSION['option']->dogovirOfertiLink ?>" target="_blank"><?= $this->text('Договором оферти') ?></a></div>
-						</label>
-					<?php } ?>
-
-					<button type="submit" class="checkout"><?= $this->text('Оформити замовлення') ?></button>
-				</div>
 			</form>
+
+			<?php if ($bonusCodes && $bonusCodes->showForm) { ?>
+				<h4><?= $this->text('Маєте купон на знижку?') ?></h4>
+				<form action="<?= SITE_URL . $_SESSION['alias']->alias ?>/coupon" method="POST" class="coupon-form text-center">
+					<div class="input-group">
+						<input type="text" name="code" placeholder="<?= $this->text('Код купону') ?>" required>
+						<label for="code"><?= $this->text('Код купону') ?></label>
+					</div>
+					<button class=""><?= $this->text('Застосувати купон') ?></button>
+				</form>
+			<?php } ?>
+
+			<div class="price-box" data-margin-top="110">
+				<?php if ($bonusCodes && !empty($bonusCodes->info)) { ?>
+					<p class="bonusCode">
+						<?= $this->text('Разом') . ': ' ?>
+						<strong class="right"><?= $subTotalFormat ?></strong>
+					</p>
+					<?php foreach ($bonusCodes->info as $key => $discount) { ?>
+						<p class="bonusCode discount">
+							<?= $this->text('Бонус-код') . ': ' . $key ?>
+							<strong class="right"><?= $discount ?></strong>
+						</p>
+					<?php }
+				}
+				if ($shippings && $shippings[0]->pay >= -1) { ?>
+					<p class="shipping">
+						<?= $this->text('Доставка') ?>
+						<strong class="right"><?= ($shippings[0]->pay == -1) ? $this->text('безкоштовно') : $shippings[0]->priceFormat ?></strong>
+					</p>
+				<?php } ?>
+
+				<?php if ($discountTotal && false) { ?>
+					<p class="discount"><?= $this->text('Ви економите') ?> <strong class="right"><?= $discountTotalFormat ?></strong></p>
+				<?php } ?>
+
+				<p class="price">
+					<?= $this->text('До оплати') ?>
+					<strong class="right"><?= $totalFormat ?></strong>
+				</p>
+
+				<?php if (!empty($_SESSION['option']->dogovirOfertiLink)) { ?>
+					<label id="oferta">
+						<input type="checkbox" name="oferta" <?= $this->userIs() ? 'checked' : '' ?> required form="confirm">
+						<i class="<?= $this->userIs() ? 'fas fa-check-square' : 'far fa-square' ?>"></i>
+						<div><?= $this->text('Я погоджуюся з') ?> <a href="<?= SERVER_URL . $_SESSION['option']->dogovirOfertiLink ?>" target="_blank"><?= $this->text('Договором оферти') ?></a></div>
+					</label>
+				<?php } ?>
+
+				<button type="submit" class="checkout" form="confirm"><?= $this->text('Оформити замовлення') ?></button>
+			</div>
 		</div>
 
 		<div class="w70-5 m-hide">
