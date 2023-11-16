052edb749d02e2edd9fdb5a84fb9e89d3c692d01
Ostap Matskiv <levso7@gmail.com>
2023-11-16 18:49:37 +0200

    __head_filters

--- app/services/shopshowcase/views/__head_filters.php
+++ app/services/shopshowcase/views/__head_filters.php
@@ -1,43 +1,34 @@
-<?php $subFilterKey = 2; $subFilterParentId = 0;
-if(!empty($filters))
+<div class="flex wrap">
+<?php if(!empty($filters))
 	foreach ($filters as $filter) {
-		if($filter->id == 2) { $subFilterKey = $filter->alias; ?>
-		<h4 class="subgroup-name">Група</h4>
-		<div class="flex h-start wrap catalog__detal">
-            <a href="<?=$this->data->get_link($filter->alias, '', 'page')?>" <?=empty($_GET[$filter->alias])?'class="active"':''?>>
-                <!-- <img src="/style/icons/catalog/component.svg" alt="all"> -->
-                <div><?=$this->text('Всі запчастини')?></div>
-            </a>
-			<?php foreach ($filter->values as $value) {
-                if(isset($_GET[$filter->alias]) && $_GET[$filter->alias] == $value->id)
-                    $subFilterParentId = empty($value->type) ? $value->id : $value->type;
-            }
+		if($filter->id == 2) {
             foreach ($filter->values as $value) {
-            if(empty($value->type)) {  ?>
-				<a href="<?=$this->data->get_link($filter->alias, $value->id, 'page')?>" <?=isset($_GET[$filter->alias]) && $subFilterParentId == $value->id ?'class="active"':''?>>
-					<?php if(!empty($value->photo)) { ?>
-			            <img src="<?=$value->photo?>" alt="<?=$value->name?>">
-			        <?php } ?>
-		            <div><?=$value->name?> <span>(<?=$value->count?>)</span></div>
-		        </a>
-        	<?php } } ?>
-        </div>
-<?php } }
-
-
-
-if(!empty($filters) && isset($_GET[$subFilterKey])) { ?>
-<h4 class="subgroup-name">Підгрупа</h4>
-<div class="flex h-start wrap catalog__detal">
-    <?php foreach ($filters as $filter) {
-        if($filter->id == 2) { 
-        foreach ($filter->values as $value) if($value->type == $subFilterParentId) { ?>
-            <a href="<?=$this->data->get_link($filter->alias, $value->id, 'page')?>" <?=isset($_GET[$filter->alias]) && ($_GET[$filter->alias] == $value->id) ?'class="active"':''?>>
-                <?php if(!empty($value->photo)) { ?>
-		            <img src="<?=$value->photo?>" alt="<?=$value->name?>">
-		        <?php } ?>
-	            <div><?=$value->name?> <span>(<?=$value->count?>)</span></div>
-            </a>
-    <?php } } } ?>
+                if(empty($value->type)) {
+                    $subFilterParentId = empty($value->type) ? $value->id : $value->type; ?>
+                    <div class="w25 m-w50">
+                        <h4 class="subgroup-name">
+                            <a href="<?=$this->data->get_link($filter->alias, $value->id, 'page')?>" <?=isset($_GET[$filter->alias]) && $_GET[$filter->alias] == $value->id ? 'class="active"':''?>>
+                                <?php if(!empty($value->photo)) { ?>
+                                    <img src="<?=$value->photo?>" alt="<?=$value->name?>">
+                                <?php } ?>
+                                <div><?=$value->name?> <span>(<?=$value->count?>)</span></div>
+                            </a>
+                        </h4>
+                        <div class="flex column v-start catalog__detal">
+                            <?php foreach ($filters as $filter_2) {
+                                if($filter_2->id == 2) { 
+                                foreach ($filter_2->values as $value) if($value->type == $subFilterParentId) { ?>
+                                    <a href="<?=$this->data->get_link($filter_2->alias, $value->id, 'page')?>" <?=isset($_GET[$filter_2->alias]) && ($_GET[$filter_2->alias] == $value->id) ? 'class="active"':''?>>
+                                        <?php /* if(!empty($value->photo)) { ?>
+                                            <img src="<?=$value->photo?>" alt="<?=$value->name?>">
+                                        <?php } */ ?>
+                                        <?=$value->name?> <span>(<?=$value->count?>)</span>
+                                    </a>
+                            <?php } } } ?>
+                        </div>
+                    </div>
+                <?php }
+            }
+        }
+    } ?>
 </div>
-<?php } ?>
