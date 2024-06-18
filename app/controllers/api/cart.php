6749e2c4b58bd8a220fceafec9528d3ee1bf25ea
Ostap Matskiv <levso7@gmail.com>
2024-06-18 20:24:18 +0300

    api/cart/ttn

---
+++ app/controllers/api/cart.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * for lifecars.com.ua
+ */
+class cart extends Controller
+{
+
+    function _remap($method)
+    {
+        if (method_exists($this, $method)) {
+            $this->$method();
+        } else {
+            $this->index($method);
+        }
+    }
+
+    function index()
+    {
+        echo 'API/cart';
+		// $this->redirect('/');
+	}
+
+    function ttn() {
+        echo '<pre>';
+        print_r($_POST);
+        echo '</pre>';
+
+        if($order_id = $this->data->post('order_id')) {
+            $data = $this->data->prepare(['ttn', 'np_status']);
+
+            if($order = $this->db->select('s_cart', 'ttn', $order_id)->get()) {
+                $log = [];
+                foreach ($data as $key => $value) {
+                    if($order[$key] != $value) {
+                        $log[] = "{$key} => {$value}";
+                    }
+                }
+                if(!empty($log)) {
+                    // $this->db->updateRow('s_cart', $data, $order_id);
+                    // $this->db->insertRow('s_cart_history', ['cart' => $order_id, 'status' => 0, 'show' => 1, 'user' => 0, 'comment' => implode(', ', $log), 'date' => time()]);
+                }
+            }
+            echo 'OK';
+        }
+        else {
+            echo 'ERROR: order not found';
+        }
+    }
+
+}
