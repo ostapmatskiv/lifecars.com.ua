50bc89e4e26670173f09bae39f8f12135cf47882
Ostap Matskiv <levso7@gmail.com>
2024-06-20 13:52:56 +0300

    api/cart/ttn

--- app/controllers/api/cart.php
+++ app/controllers/api/cart.php
@@ -22,32 +22,52 @@ class cart extends Controller
 	}
 
     function ttn() {
+        /* Array
+        (
+            [0] => Array
+                (
+                    [order_id] => 3914
+                    [ttn] => 20450938610386
+                    [np_status] => Прибув у відділення
+                )
+
+        ) */
+
         $content = file_get_contents('php://input');
+        if(empty($content)) {
+            echo 'ERROR: empty input content';
+            return;
+        }
         $content_json = json_decode($content, true);
-        echo '<pre>';
-        print_r($content);
-        print_r($content_json);
-        echo '</pre>';
+        if(empty($content_json)) {
+            echo 'ERROR: empty content_json';
+            return;
+        }
+        // echo '<pre>';
+        // print_r($content);
+        // print_r($content_json);
+        // echo '</pre>';
 
-        if($order_id = $this->data->post('order_id')) {
-            $data = $this->data->prepare(['ttn', 'np_status']);
-
-            if($order = $this->db->select('s_cart', 'ttn', $order_id)->get()) {
-                $log = [];
-                foreach ($data as $key => $value) {
-                    if($order[$key] != $value) {
-                        $log[] = "{$key} => {$value}";
+        foreach ($content_json as $row) {
+            if($order_id = $row['order_id']) {
+                $data = ['ttn' => $row['ttn'], 'np_status' => $row['np_status']];
+                if($order = $this->db->select('s_cart', 'ttn, np_status', $order_id)->get()) {
+                    $log = [];
+                    foreach ($data as $key => $value) {
+                        if($order->$key != $value) {
+                            $log[] = "{$key} => {$value}";
+                        }
                     }
+                    if(!empty($log)) {
+                        $this->db->updateRow('s_cart', $data, $order_id);
+                        $this->db->insertRow('s_cart_history', ['cart' => $order_id, 'status' => 0, 'show' => 1, 'user' => 0, 'comment' => implode(', ', $log), 'date' => time()]);
+                    }
+                    echo "OK: order {$order_id} updated. " . PHP_EOL;
                 }
-                if(!empty($log)) {
-                    // $this->db->updateRow('s_cart', $data, $order_id);
-                    // $this->db->insertRow('s_cart_history', ['cart' => $order_id, 'status' => 0, 'show' => 1, 'user' => 0, 'comment' => implode(', ', $log), 'date' => time()]);
+                else {
+                    echo "ERROR: order {$order_id} not found. " . PHP_EOL;
                 }
             }
-            echo 'OK';
-        }
-        else {
-            echo 'ERROR: order not found';
         }
     }
 
